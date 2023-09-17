d6e1fd60b9cd33c435ff8cdc576f804a2ec97695
Shist <shist.pupust@mail.ru>
2023-09-18 01:49:37 +0300

    Added deleting hero by cross-btn (from json-server and state)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,30 @@
 export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+  return {
+    type: "HEROES_FETCHING",
+  };
+};
 
 export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+  return {
+    type: "HEROES_FETCHED",
+    payload: heroes,
+  };
+};
 
 export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+  return {
+    type: "HEROES_FETCHING_ERROR",
+  };
+};
+
+export const heroesDeletingError = () => {
+  return {
+    type: "HEROES_DELETING_ERROR",
+  };
+};
+
+export const heroesSendingError = () => {
+  return {
+    type: "HEROES_SENDING_ERROR",
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,40 @@
 const initialState = {
-    heroes: [],
-    heroesLoadingStatus: 'idle',
-    filters: []
-}
+  heroes: [],
+  heroesLoadingStatus: "idle",
+  filters: [],
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
-        default: return state
-    }
-}
+  switch (action.type) {
+    case "HEROES_FETCHING":
+      return {
+        ...state,
+        heroesLoadingStatus: "loading",
+      };
+    case "HEROES_FETCHED":
+      return {
+        ...state,
+        heroes: action.payload,
+        heroesLoadingStatus: "idle",
+      };
+    case "HEROES_FETCHING_ERROR":
+      return {
+        ...state,
+        heroesLoadingStatus: "fetch-error",
+      };
+    case "HEROES_DELETING_ERROR":
+      return {
+        ...state,
+        heroesLoadingStatus: "delete-error",
+      };
+    case "HEROES_SENDING_ERROR":
+      return {
+        ...state,
+        heroesLoadingStatus: "send-error",
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
